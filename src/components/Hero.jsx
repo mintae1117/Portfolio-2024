a3b1ba7b8ae6c8edcb994b3a9ca41919be749245
mintaeKim <peterzzo@naver.com>
2024-04-29 05:08:11 +0900

    scroll down css change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 left-10 w-full flex items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -59,6 +59,7 @@ const Hero = () => {
             />
           </div>
         </a>
+        <p style={{marginLeft:"10px"}}>Scroll down ⬇️</p>
       </div>
     </section>
   );
