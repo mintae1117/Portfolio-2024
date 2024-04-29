380a7ebcf2b3a2802b2c062afb698d82c0b0d273
mintaeKim <peterzzo@naver.com>
2024-04-29 22:07:19 +0900

    bg change final

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/neonbg.jpg')",
+        "hero-pattern": "url('/src/assets/pxfuel.jpg')",
       },
     },
   },
