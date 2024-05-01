b28a7f12ae25509de096978741849335388de37c
mintaeKim <peterzzo@naver.com>
2024-05-01 19:11:38 +0900

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,9 +42,9 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-28 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            My Portfolio
+            Portfolio
           </p>
         </Link>
 
