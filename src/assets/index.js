9ac246a39c2dc7d4cadb8488303e52243f6acde7
mintaeKim <peterzzo@naver.com>
2024-05-20 13:36:36 +0900

    tech edit end

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,12 +6,10 @@ import nomad from "./nomad.svg";
 import instagram from "./instagram.avif";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
@@ -40,12 +38,10 @@ export {
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
   redux,
